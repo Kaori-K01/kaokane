e6d4505c8999387536a945a7206ab019a041d1a8
Kaori-K01 <kaonori0126@gmail.com>
2026-08-08 21:19:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kaokane-v0.3';
+const CACHE='kaokane-v0.4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(
